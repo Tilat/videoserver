3c74a30b99c1c55aa3da61068db1a91a2973ed1c
Sergey Grodzitskiy <sergeygrodzitski@odesk.com>
2014-12-06 00:17:35 +0300

    [sergey] working on connector

--- apps/rtsp/src/rtsp_sup.erl
+++ apps/rtsp/src/rtsp_sup.erl
@@ -18,22 +18,21 @@ start_link() ->
 %% ===================================================================
 %% Supervisor callbacks
 %% ===================================================================
-
 init([]) ->
-  {ok, Port} = {ok, 8554},
-  {ok, RTSPSocket} = gen_tcp:listen(Port, [{active,once}, {packet,line} ]),
-  spawn_link(fun empty_listeners/0),
-  {ok, {{simple_one_for_one, 60, 3600},
-    [{rtsp_connector,
-      {rtsp_connector, start_link, [RTSPSocket]}, % pass the socket!
-      temporary, 1000, worker, [rtsp_connector]}
-    ]}
-  }.
-
+{ok, Port} = {ok,8554},
+%% Set the socket into {active_once} mode.
+%% See sockserv_serv comments for more details
+{ok, ListenSocket} = gen_tcp:listen(Port, [{active,once}, {packet,line}]),
+spawn_link(fun empty_listeners/0),
+{ok, {{simple_one_for_one, 60, 3600},
+[{sockserv_serv,
+{sockserv_serv, start_link, [ListenSocket]}, % pass the socket!
+temporary, 1000, worker, [sockserv_serv]}
+]}}.
 
 start_socket() ->
-  supervisor:start_child(?MODULE, []).
+supervisor:start_child(?MODULE, []).
 
 empty_listeners() ->
-  [start_socket() || _ <- lists:seq(1,5)],
-  ok.
+[start_socket() || _ <- lists:seq(1,20)],
+ok.
