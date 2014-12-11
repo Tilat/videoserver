56ffce0766753b1820b0880a66b3aa2ddb08cdcb
Sergey Grodzitskiy <sergeygrodzitski@odesk.com>
2014-12-11 16:31:16 +0300

    [sergey] test socket connector

--- apps/rtsp/src/rtsp_connector.erl
+++ apps/rtsp/src/rtsp_connector.erl
@@ -2,71 +2,73 @@
 -behaviour(gen_server).
 -define(SERVER, ?MODULE).
 
--record(session, {id, % id
-  socket}). % the current socket
+
+-record(state, {id, socket}).
+
 
 -import(error_logger, [info_msg/1, info_msg/2]).
-%% ------------------------------------------------------------------
-%% API Function Exports
-%% ------------------------------------------------------------------
+
 
 -export([start_link/1]).
 
-%% ------------------------------------------------------------------
-%% gen_server Function Exports
-%% ------------------------------------------------------------------
 
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
   terminate/2, code_change/3]).
 
-%% ------------------------------------------------------------------
-%% API Function Definitions
-%% ------------------------------------------------------------------
+
+
 start_link(Socket) ->
-gen_server:start_link(?MODULE, Socket, []).
+	gen_server:start_link(?MODULE, Socket, []).
+
+
 
 init(Socket) ->
 %% properly seeding the process
 %% Because accepting a connection is a blocking function call,
 %% we can not do it in here. Forward to the server loop!
-gen_server:cast(self(), accept),
-{ok, #state{socket=Socket}}.
+	gen_server:cast(self(), accept),
+	{ok, #state{socket=Socket}}.
 
 
-%% We never need you, handle_call!
+
 handle_call(_E, _From, State) ->
-{noreply, State}.
+	{noreply, State}.
+
 
 
 handle_cast(accept, S = #state{socket=ListenSocket}) ->
-{ok, AcceptSocket} = gen_tcp:accept(ListenSocket),
-%% Remember that thou art dust, and to dust thou shalt return.
-%% We want to always keep a given number of children in this app.
-socktest_sup:start_socket(), % a new acceptor is born, praise the lord
-ok = inet:setopts(AcceptSocket, [{active, once}]),
-%send(AcceptSocket, "hello", []),
-{noreply, S#state{socket=AcceptSocket, next=name}}.
+	{ok, AcceptSocket} = gen_tcp:accept(ListenSocket),
+	%% Remember that thou art dust, and to dust thou shalt return.
+	%% We want to always keep a given number of children in this app.
+	rtsp_sup:start_socket(), % a new acceptor is born, praise the lord
+	ok = inet:setopts(AcceptSocket, [{active, once}]),	
+	{noreply, S#state{socket=AcceptSocket}}.
+
+
 
 handle_info({tcp_closed, _Socket}, S) ->
-{stop, normal, S};
+	{stop, normal, S};
+
 handle_info({tcp_error, _Socket, _}, S) ->
-{stop, normal, S};
+	{stop, normal, S};
+
 handle_info({tcp, Socket, Str}, S = #state{socket=Socket}) ->
-send(Socket, "response", []),
-{noreply, S}.
-%;
-%handle_info(M,S) ->
-%io:format("unexpected: ~p~n", [M]),
-%{noreply, S}.
+	send(Socket, "response", []),
+	{noreply, S}.
+
 
 
 terminate(_Reason, _State) ->
-io:format("terminate reason: ~p~n", [_Reason]).
+	io:format("terminate reason: ~p~n", [_Reason]).
+
+
 
 code_change(_OldVsn, State, _Extra) ->
-{ok, State}.
+	{ok, State}.
+
+
 
 send(Socket, Str, Args) ->
-ok = gen_tcp:send(Socket, io_lib:format(Str++"~n", Args)),
-ok = inet:setopts(Socket, [{active, once}]),
-ok.
+	ok = gen_tcp:send(Socket, io_lib:format(Str++"~n", Args)),
+	ok = inet:setopts(Socket, [{active, once}]),
+	ok.
--- apps/rtsp/src/rtsp_sup.erl
+++ apps/rtsp/src/rtsp_sup.erl
@@ -2,37 +2,32 @@
 
 -behaviour(supervisor).
 
-%% API
--export([start_link/0,start_socket/0]).
-
-%% Supervisor callbacks
 -export([init/1]).
 
-%% ===================================================================
-%% API functions
-%% ===================================================================
+-export([start_link/0,start_socket/0]).
+
 
 start_link() ->
-  supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+	supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
-%% ===================================================================
-%% Supervisor callbacks
-%% ===================================================================
+
 init([]) ->
-{ok, Port} = {ok,8554},
-%% Set the socket into {active_once} mode.
-%% See sockserv_serv comments for more details
-{ok, ListenSocket} = gen_tcp:listen(Port, [{active,once}, {packet,line}]),
-spawn_link(fun empty_listeners/0),
-{ok, {{simple_one_for_one, 60, 3600},
-[{sockserv_serv,
-{sockserv_serv, start_link, [ListenSocket]}, % pass the socket!
-temporary, 1000, worker, [sockserv_serv]}
-]}}.
+	{ok, Port} = {ok,8554},
+	{ok, ListenSocket} = gen_tcp:listen(Port, [{active,once}, {packet,line}]),
+	spawn_link(fun empty_listeners/0),
+		{ok, {{simple_one_for_one, 60, 3600},
+			[{rtsp_socket_handler,
+				{rtsp_connector, start_link, [ListenSocket]}, % module name, function, argument
+				temporary,
+				1000, 
+				worker,
+				[rtsp_connector]}
+			]
+		}}.
 
 start_socket() ->
-supervisor:start_child(?MODULE, []).
+	supervisor:start_child(?MODULE, []).
 
 empty_listeners() ->
-[start_socket() || _ <- lists:seq(1,20)],
+	[start_socket() || _ <- lists:seq(1,20)],
 ok.
